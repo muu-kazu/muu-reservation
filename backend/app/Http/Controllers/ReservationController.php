ad920ac0011a2820727d16c316025d66166eda23
muu-ima <enyukari.k.imamura@email.com>
2025-09-05 11:44:20 +0900

    Use JSON_UNESCAPED_UNICODE for ReservationController responses

--- backend/app/Http/Controllers/ReservationController.php
+++ backend/app/Http/Controllers/ReservationController.php
@@ -9,9 +9,12 @@ class ReservationController extends Controller
 {
     public function index() {
         return Reservation::all(); // 全件取得
+        // ★ JSON_UNESCAPED_UNICODE を指定
+        return response()->json($reservations, 200, [], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
     }
 
     public function show($id) {
         return Reservation::findOrFail($id); // id指定で取得
+        return response()->json($reservation, 200, [], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
     }
 }
